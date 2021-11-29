c76c827a338df4d97e7e489eeb4fdc32dc50310e
Amir Ayoub <techamir25@gmail.com>
2021-11-28 20:55:17 -0500

    final

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/prince/Web-development-projects/Git/django-react-ecommerce/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
